a349c6bd15b5dfdbf16dca4a81a90d1143731c18
LeeSin <leesin2011@foxmail.com>
2017-08-22 19:02:30 +0800

    run framework

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+
+    public function bike()
+    {
+    	die('s');
+        return view();
+    }
+}
